644d32d43f1de2e9678497ae7ce64dfb71059bf0
Maverick Tesini <mavericktesini@gmail.com>
2018-01-19 18:36:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
-var kittens = ["Milo", "Otis", "Garfield"];
+var kittens = ["Milo", "Otis", "Garfield", "Ralph"];
 function destructivelyAppendKitten(name) {
-  kittens.shift(name);
+  kittens.unshift(name);
   return kittens;
 }
